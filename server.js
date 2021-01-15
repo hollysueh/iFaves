7a6e198024af494c65f6b6387aaa4fc510bc7be6
Holly Henaghan <hollyhenaghan@gmail.com>
2021-01-15 21:21:57 +0200

    f

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ const app = require('./app.js')
   res.sendFile(path.resolve(__dirname, "ifaves_frontend", "build", "index.html")); });
   } */
 
-  if (process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'ifaves_frontend/build')));
-    app.get('*',(req,res)=> {
-      res.sendFile(path.resolve(__dirname, 'ifaves_frontend', 'build','index.html'));
-    });
-  } 
-
 //Listen on port 8081
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
