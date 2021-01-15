34aff4eff824e06de21ae81e67b6c14a181bf279
Holly Henaghan <hollyhenaghan@gmail.com>
2021-01-15 21:08:34 +0200

    d

--- app.js
+++ app.js
@@ -76,12 +76,14 @@ app.delete("/favesList", (req, res) => {
 })
 
 // For app deployment on Heroku
-/* if (process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'iFaves_frontend/build')));
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, 'ifaves_frontend/build')));
   app.get('*',(req,res)=> {
-    res.sendFile(path.resolve(__dirname, 'iFaves_frontend', 'build','index.html'));
+    res.sendFile(path.resolve(__dirname, 'ifaves_frontend', 'build','index.html'));
   });
-} */
+}
+
+app.use(express.static(path.join(__dirname,'frontend/build')));
 
 module.exports = app
 
