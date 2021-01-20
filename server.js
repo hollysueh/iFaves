d85e0710df6305f5098e436717d680223b8e994f
Holly Henaghan <hollyhenaghan@gmail.com>
2021-01-20 15:00:03 +0200

    test

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = require('./app.js')
 
 //For Heroku deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("iFaves_frontend/build"));
+  app.use(express.static("ifaves_frontend/build"));
   app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "iFaves_frontend", "build", "index.html")); });
+  res.sendFile(path.resolve(__dirname, "ifaves_frontend", "build", "index.html")); });
   }
 
 //Listen on port 8081
