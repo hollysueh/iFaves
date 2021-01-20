382f7a44ac7f0b2c595b058d85e47776adf23aad
Holly Henaghan <hollyhenaghan@gmail.com>
2021-01-20 15:48:31 +0200

    test

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const path = require('path');
 const app = require('./app.js')
 
 //For Heroku deployment
+
+//static file
+app.use(express.static(path.join(__dirname, "iFaves_frontend/build")));
+
+//production mode
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("iFaves_frontend/build"));
+  app.use(express.static(path.join(__dirname, "iFaves_frontend/build"));
   app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "iFaves_frontend", "build", "index.html")); });
-  }
+  res.sendFile(path.join(__dirname, "iFaves_frontend", "build", "index.html")); });
+}
+
+//build mode
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/iFaves_frontend/public/index.html'));})
+
 
 //Listen on port 8081
 const PORT = process.env.PORT || 8081;
